172b08e6020fc1486dbb9fb1a5505699e39d73a2
Saniga Babu <sanigababu007@gmail.com>
2025-06-17 14:36:47 +0530

    Add custom startup script for Azure

--- startup.sh
+++ startup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-pip install -r requirements.txt
-exec uvicorn main:app --host=0.0.0.0 --port=8000
+
+echo "🔁 Installing Python dependencies..."
+pip install --target=".python_packages/lib/site-packages" -r requirements.txt
+
+echo "🔁 Exporting Python path..."
+export PYTHONPATH="$PYTHONPATH:.python_packages/lib/site-packages"
+
+echo "🚀 Starting FastAPI app with Uvicorn..."
+exec python -m uvicorn main:app --host=0.0.0.0 --port=8000

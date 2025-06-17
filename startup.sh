f885e2d3da70caac87b28a644dbe3f62b18a4af7
Saniga Babu <sanigababu007@gmail.com>
2025-06-17 14:06:58 +0530

    Add startup script to fix uvicorn not found

--- startup.sh
+++ startup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Install Python dependencies at runtime (in case Azure didn't preserve them)
 pip install -r requirements.txt
-
-# Run your FastAPI app with uvicorn
 exec uvicorn main:app --host=0.0.0.0 --port=8000
